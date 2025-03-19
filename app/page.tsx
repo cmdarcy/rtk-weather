6c59e2c14e29118828cef96cc3b62e6dc43c551a
Christopher d'Arcy <cmdarcy89@gmail.com>
2025-03-18 21:52:47 -0400

    style theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,9 @@ export default function Home() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
         RTK Weather
       </h1>
-      <ModeToggle />
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
       <ShadSearchForm />
       <Forecast />
     </main>
